31fc833392837ac708347472105ab89ba02b582d
niklauslee <niklaus.lee@gmail.com>
2024-02-07 13:42:14 +0900

    build for esmodule

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src/index.ts"],
-  format: ["cjs", "esm"], // Build for commonJS and ESmodules
+  format: ["esm"], // Build for ESmodules
   dts: true, // Generate declaration file (.d.ts)
   splitting: false,
   sourcemap: true,
