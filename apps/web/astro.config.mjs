ea5edd8faa4f86ded7404d291071f8087b8de0c5
niklauslee <niklaus.lee@gmail.com>
2024-04-16 17:20:06 +0900

    Update web repo

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -1,7 +1,15 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    sitemap(),
+  ],
 });
---
+++ apps/web/src/components/icons/logo.tsx
@@ -0,0 +1,53 @@
+import { type IconProps } from "./types";
+
+const SvgLogo = ({ size = 24, strokeWidth = 2, ...props }: IconProps) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 24 24"
+    width={size}
+    height={size}
+    fill="none"
+    {...props}
+  >
+    <path d="M20 8V16" stroke="currentColor" strokeWidth={strokeWidth} />
+    <path d="M4 8V16" stroke="currentColor" strokeWidth={strokeWidth} />
+    <rect
+      x="1"
+      y="17"
+      width="6"
+      height="6"
+      stroke="currentColor"
+      strokeWidth={strokeWidth}
+    />
+    <rect
+      x="17"
+      y="17"
+      width="6"
+      height="6"
+      stroke="currentColor"
+      strokeWidth={strokeWidth}
+    />
+    <rect
+      x="17"
+      y="1"
+      width="6"
+      height="6"
+      stroke="currentColor"
+      strokeWidth={strokeWidth}
+    />
+    <rect
+      x="1"
+      y="1"
+      width="6"
+      height="6"
+      stroke="currentColor"
+      strokeWidth={strokeWidth}
+    />
+    <path d="M4 14L14 4" stroke="currentColor" strokeWidth={strokeWidth} />
+    <path d="M6 18L18 6" stroke="currentColor" strokeWidth={strokeWidth} />
+    <path d="M10 20L20 10" stroke="currentColor" strokeWidth={strokeWidth} />
+    <path d="M8 4H16" stroke="currentColor" strokeWidth={strokeWidth} />
+    <path d="M8 20H16" stroke="currentColor" strokeWidth={strokeWidth} />
+  </svg>
+);
+export default SvgLogo;
